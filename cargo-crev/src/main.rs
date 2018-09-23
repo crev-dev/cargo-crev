b910aa2f85e166c0f5fc239d56562322308f1c0b
Dawid Ciężarkiewicz <dpc@dpc.pw>
2018-09-22 18:40:18 -0700

    Add skel for `cargo crev`

---
+++ cargo-crev/src/main.rs
@@ -0,0 +1,107 @@
+#![allow(deprecated)]
+//#[macro_use]
+
+#[macro_use]
+extern crate quicli;
+use crev_common;
+
+use rprompt;
+#[macro_use]
+extern crate structopt;
+
+use common_failures::prelude::*;
+use crev_data::id::OwnId;
+use crev_lib::{id::LockedId, local::Local, repo::Repo};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+mod opts;
+mod util;
+
+fn show_id() -> Result<()> {
+    let local = Local::auto_open()?;
+    let id = local.read_locked_id()?;
+    let id = id.to_pubid();
+    print!("{}", id);
+    Ok(())
+}
+
+fn gen_id() -> Result<()> {
+    eprintln!("Crev relies on personal, publicly accessible repositories to circulate proofs.");
+    eprintln!("Enter public git address you're planing to use for your CrevID.");
+    eprintln!("E.g.: https://github.com/<myusername>/crev-proofs");
+    eprintln!("Changing it later will require manual config file editing.");
+    let mut url;
+    loop {
+        url = rprompt::prompt_reply_stdout("Git URL: ")?;
+        eprintln!("");
+        eprintln!("You've entered: {}", url);
+        if crev_common::yes_or_no_was_y("Is this correct? (y/n) ")? {
+            break;
+        }
+    }
+
+    let id = OwnId::generate(url);
+    eprintln!("Your CrevID will be protected by a passphrase.");
+    eprintln!("There's no way to recover your CrevID if you forget your passphrase.");
+    let passphrase = util::read_new_passphrase()?;
+    let locked = LockedId::from_own_id(&id, &passphrase)?;
+
+    let local = Local::auto_create()?;
+    local.save_locked_id(&locked)?;
+    local.save_current_id(&id)?;
+
+    eprintln!("Your CrevID was created and will be printed blow in encrypted form.");
+    eprintln!("Make sure to back it up on another device, to prevent loosing it.");
+
+    println!("{}", locked);
+    Ok(())
+}
+
+main!(|opts: opts::Opts| match opts.command {
+    opts::Command::Id(id) => match id.id_command {
+        opts::IdCommand::Show => show_id()?,
+        opts::IdCommand::Gen => gen_id()?,
+    },
+    opts::Command::Trust(trust) => match trust {
+        opts::Trust::Add(trust) => {
+            let local = Local::auto_open()?;
+            let passphrase = util::read_passphrase()?;
+            local.trust_ids(trust.pub_ids, passphrase)?;
+        }
+        opts::Trust::Update => {
+            let local = Local::auto_open()?;
+            local.trust_update()?;
+        }
+    },
+    opts::Command::Add(add) => {
+        let mut repo = Repo::auto_open()?;
+        repo.add(add.paths)?;
+    }
+    opts::Command::Commit(opts) => {
+        let mut repo = Repo::auto_open()?;
+        let passphrase = util::read_passphrase()?;
+        if opts.all {
+            repo.commit_all(passphrase, opts.allow_dirty)?;
+        } else {
+            repo.commit(passphrase, opts.allow_dirty)?;
+        }
+    }
+    opts::Command::Init => {
+        let local = Local::auto_open()?;
+        let cur_id = local.read_current_id()?;
+        Repo::init(PathBuf::from(".".to_string()), cur_id)?;
+    }
+    opts::Command::Status => {
+        let mut repo = Repo::auto_open()?;
+        repo.status()?;
+    }
+    opts::Command::Remove(remove) => {
+        let mut repo = Repo::auto_open()?;
+        repo.remove(remove.paths)?;
+    }
+    opts::Command::Verify(_verify_opts) => {
+        let mut repo = Repo::auto_open()?;
+        repo.verify()?;
+    }
+});
---
+++ cargo-crev/src/opts.rs
@@ -0,0 +1,102 @@
+use std::path::PathBuf;
+
+#[derive(Debug, StructOpt, Clone)]
+pub struct Id {
+    #[structopt(subcommand)]
+    pub id_command: IdCommand,
+}
+
+#[derive(Debug, StructOpt, Clone)]
+pub enum IdCommand {
+    #[structopt(name = "gen")]
+    /// Generate a CrevID
+    Gen,
+    #[structopt(name = "show")]
+    /// Show CrevID information
+    Show,
+}
+
+#[derive(Debug, StructOpt, Clone)]
+pub struct Add {
+    #[structopt(parse(from_os_str))]
+    /// Paths to add
+    pub paths: Vec<PathBuf>,
+}
+
+#[derive(Debug, StructOpt, Clone)]
+pub struct Remove {
+    #[structopt(parse(from_os_str))]
+    /// Paths to remove
+    pub paths: Vec<PathBuf>,
+}
+
+#[derive(Debug, StructOpt, Clone)]
+pub struct TrustAdd {
+    /// Public IDs to create Trust Proof for
+    pub pub_ids: Vec<String>,
+}
+
+#[derive(Debug, StructOpt, Clone)]
+pub enum Trust {
+    #[structopt(name = "add")]
+    /// Create a new Trust Proof
+    Add(TrustAdd),
+    #[structopt(name = "update")]
+    /// Update trustdb by fetching updates from trusted sources
+    Update,
+}
+
+#[derive(Debug, StructOpt, Clone)]
+pub struct Verify {
+    depth: u64,
+    #[structopt(long = "high-cost")]
+    high_cost: u64,
+    #[structopt(long = "medium-cost")]
+    medium_cost: u64,
+    #[structopt(long = "low-cost")]
+    low_cost: u64,
+}
+
+#[derive(Debug, StructOpt, Clone)]
+pub struct Commit {
+    #[structopt(long = "all", short = "a")]
+    pub all: bool,
+    #[structopt(long = "allow-dirty")]
+    pub allow_dirty: bool,
+}
+
+#[derive(Debug, StructOpt, Clone)]
+pub enum Command {
+    #[structopt(name = "id")]
+    /// CrevID management
+    Id(Id),
+    #[structopt(name = "add")]
+    /// Add paths to reviewed list
+    Add(Add),
+    /// Create a new Review Proof from reviewed list
+    #[structopt(name = "commit")]
+    Commit(Commit),
+    #[structopt(name = "init")]
+    /// Init `.crev` directory
+    Init,
+    #[structopt(name = "status")]
+    /// Display pending review list
+    Status,
+    #[structopt(name = "rm")]
+    /// Remove path from reviewed list
+    Remove(Remove),
+    /// Verify review coverage of the project
+    Verify(Verify),
+    #[structopt(name = "trust")]
+    /// Trust Store management
+    Trust(Trust),
+}
+
+#[derive(Debug, StructOpt, Clone)]
+#[structopt(name = "crev", about = "Distributed code review system")]
+pub struct Opts {
+    #[structopt(subcommand)]
+    pub command: Command,
+    //    #[structopt(flatten)]
+    //    verbosity: Verbosity,
+}
