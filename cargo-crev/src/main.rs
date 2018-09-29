d611abd7024311d614cc4dcb933dc31cc64ede4e
Dawid Ciężarkiewicz <dpc@dpc.pw>
2018-09-28 21:31:44 -0700

    Skeleton of simple `cargo crev` command

--- cargo-crev/src/main.rs
+++ cargo-crev/src/main.rs
@@ -3,9 +3,7 @@
 
 #[macro_use]
 extern crate quicli;
-use crev_common;
 
-use rprompt;
 #[macro_use]
 extern crate structopt;
 
@@ -16,92 +14,27 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 mod opts;
-mod util;
 
-fn show_id() -> Result<()> {
-    let local = Local::auto_open()?;
-    let id = local.read_locked_id()?;
-    let id = id.to_pubid();
-    print!("{}", id);
-    Ok(())
+fn download_all_deps() -> Result<Vec<String>> {
+    unimplemented!();
 }
 
-fn gen_id() -> Result<()> {
-    eprintln!("Crev relies on personal, publicly accessible repositories to circulate proofs.");
-    eprintln!("Enter public git address you're planing to use for your CrevID.");
-    eprintln!("E.g.: https://github.com/<myusername>/crev-proofs");
-    eprintln!("Changing it later will require manual config file editing.");
-    let mut url;
-    loop {
-        url = rprompt::prompt_reply_stdout("Git URL: ")?;
-        eprintln!("");
-        eprintln!("You've entered: {}", url);
-        if crev_common::yes_or_no_was_y("Is this correct? (y/n) ")? {
-            break;
-        }
-    }
+fn possibly_download_updates(_dep_name: &str) -> Result<()> {
+    unimplemented!();
+}
 
-    let id = OwnId::generate(url);
-    eprintln!("Your CrevID will be protected by a passphrase.");
-    eprintln!("There's no way to recover your CrevID if you forget your passphrase.");
-    let passphrase = util::read_new_passphrase()?;
-    let locked = LockedId::from_own_id(&id, &passphrase)?;
-
-    let local = Local::auto_create()?;
-    local.save_locked_id(&locked)?;
-    local.save_current_id(&id)?;
-
-    eprintln!("Your CrevID was created and will be printed blow in encrypted form.");
-    eprintln!("Make sure to back it up on another device, to prevent loosing it.");
-
-    println!("{}", locked);
-    Ok(())
+fn verify_dependency(_dep_name: &str) -> Result<()> {
+    unimplemented!();
 }
 
 main!(|opts: opts::Opts| match opts.command {
-    opts::Command::Id(id) => match id.id_command {
-        opts::IdCommand::Show => show_id()?,
-        opts::IdCommand::Gen => gen_id()?,
-    },
-    opts::Command::Trust(trust) => match trust {
-        opts::Trust::Add(trust) => {
-            let local = Local::auto_open()?;
-            let passphrase = util::read_passphrase()?;
-            local.trust_ids(trust.pub_ids, passphrase)?;
-        }
-        opts::Trust::Update => {
-            let local = Local::auto_open()?;
-            local.fetch_updates()?;
-        }
-    },
-    opts::Command::Add(add) => {
-        let mut repo = Repo::auto_open()?;
-        repo.add(add.paths)?;
-    }
-    opts::Command::Commit(opts) => {
-        let mut repo = Repo::auto_open()?;
-        let passphrase = util::read_passphrase()?;
-        if opts.all {
-            repo.trust_project(passphrase, opts.allow_dirty)?;
-        } else {
-            repo.commit(passphrase, opts.allow_dirty)?;
-        }
-    }
-    opts::Command::Init => {
-        let local = Local::auto_open()?;
-        let cur_id = local.read_current_id()?;
-        Repo::init(PathBuf::from(".".to_string()), cur_id)?;
-    }
-    opts::Command::Status => {
-        let mut repo = Repo::auto_open()?;
-        repo.status()?;
-    }
-    opts::Command::Remove(remove) => {
-        let mut repo = Repo::auto_open()?;
-        repo.remove(remove.paths)?;
-    }
     opts::Command::Verify(_verify_opts) => {
-        let mut repo = Repo::auto_open()?;
-        repo.verify()?;
+        let list_of_deps = download_all_deps()?;
+        for dep_name in &list_of_deps {
+            possibly_download_updates(&dep_name)?;
+        }
+        for dep_name in &list_of_deps {
+            verify_dependency(&dep_name)?;
+        }
     }
 });
