abdc3d8130e8d54c3060714ff17d6a23463e7a2d
Dawid Ciężarkiewicz <dpc@dpc.pw>
2019-01-08 21:30:41 -0800

    Refactoring & cleanup

--- cargo-crev/src/main.rs
+++ cargo-crev/src/main.rs
@@ -28,11 +28,6 @@ mod term;
 use crev_data::proof;
 use crev_lib::TrustOrDistrust::{self, *};
 
-struct Repo {
-    manifest_path: PathBuf,
-    config: cargo::util::config::Config,
-}
-
 /// Name of ENV with original location `crev goto` was called from
 const GOTO_ORIGINAL_DIR_ENV: &str = "CARGO_CREV_GOTO_ORIGINAL_DIR";
 /// Name of ENV with name of the crate that we've `goto`ed to
@@ -46,6 +41,7 @@ const KNOWN_CARGO_OWNERS_FILE: &str = "known_cargo_owners.txt";
 /// Constant we use for `source` in the review proof
 const PROJECT_SOURCE_CRATES_IO: &str = "https://crates.io";
 
+/// The file added to crates containing vcs revision
 const VCS_INFO_JSON_FILE: &str = ".cargo_vcs_info.json";
 
 /// Data from `.cargo_vcs_info.json`
@@ -77,6 +73,8 @@ impl VcsInfoJson {
         Some(s.to_string())
     }
 }
+
+/// TODO: Get rid of this
 #[derive(Debug)]
 struct KnownOwnersColored(usize);
 
@@ -96,6 +94,12 @@ impl crev_lib::Colored for KnownOwnersColored {
     }
 }
 
+/// A handle to the current Rust project
+struct Repo {
+    manifest_path: PathBuf,
+    config: cargo::util::config::Config,
+}
+
 impl Repo {
     fn auto_open_cwd() -> Result<Self> {
         cargo::core::enable_nightly_features();
@@ -109,7 +113,8 @@ impl Repo {
         })
     }
 
-    fn for_every_non_local_dependency_dir(
+    /// Run `f` for every non-local dependency crate
+    fn for_every_non_local_dep_crate(
         &self,
         mut f: impl FnMut(&Package) -> Result<()>,
     ) -> Result<()> {
@@ -148,7 +153,7 @@ impl Repo {
         &self,
         name: &str,
         version: Option<&str>,
-    ) -> Result<Option<(PathBuf, semver::Version)>> {
+    ) -> Result<Option<Package>> {
         let map = cargo::sources::SourceConfigMap::new(&self.config)?;
         let source_id = SourceId::crates_io(&self.config)?;
         let mut source = map.load(&source_id)?;
@@ -180,24 +185,19 @@ impl Repo {
             &self.config,
         )?;
         let pkg_id = summary.package_id();
-        let pkg = package_set.get_one(pkg_id)?;
 
-        Ok(Some((pkg.root().to_owned(), pkg_id.version().to_owned())))
+        Ok(Some(package_set.get_one(pkg_id)?.to_owned()))
     }
 
-    fn find_dependency_dir(
-        &self,
-        name: &str,
-        version: Option<&str>,
-    ) -> Result<Option<(PathBuf, semver::Version)>> {
+    fn find_dependency(&self, name: &str, version: Option<&str>) -> Result<Option<Package>> {
         let mut ret = vec![];
 
-        self.for_every_non_local_dependency_dir(|pkg| {
+        self.for_every_non_local_dep_crate(|pkg| {
             let pkg_id = pkg.package_id();
             if name == pkg_id.name().as_str()
                 && (version.is_none() || version == Some(&pkg_id.version().to_string()))
             {
-                ret.push((pkg.root().to_owned(), pkg_id.version().to_owned()));
+                ret.push(pkg.to_owned());
             }
             Ok(())
         })?;
@@ -209,21 +209,17 @@ impl Repo {
         }
     }
 
-    fn find_crate(
-        &self,
-        name: &str,
-        version: Option<&str>,
-        independent: bool,
-    ) -> Result<(PathBuf, semver::Version)> {
+    fn find_crate(&self, name: &str, version: Option<&str>, independent: bool) -> Result<Package> {
         if independent {
             self.find_idependent_crate_dir(name, version)?
         } else {
-            self.find_dependency_dir(name, version)?
+            self.find_dependency(name, version)?
         }
         .ok_or_else(|| format_err!("Could not find requested crate"))
     }
 }
 
+/// Ignore things that are commonly added during the review (eg. by RLS)
 fn cargo_full_ignore_list() -> HashSet<PathBuf> {
     let mut ignore_list = HashSet::new();
     ignore_list.insert(PathBuf::from(".cargo-ok"));
@@ -232,12 +228,17 @@ fn cargo_full_ignore_list() -> HashSet<PathBuf> {
     ignore_list
 }
 
+/// Ignore only the marker added by `cargo` after fully downloading and extracting crate
 fn cargo_min_ignore_list() -> HashSet<PathBuf> {
     let mut ignore_list = HashSet::new();
     ignore_list.insert(PathBuf::from(".cargo-ok"));
     ignore_list
 }
 
+/// `cd` into crate source code and start shell
+///
+/// Set some `envs` to help other commands work
+/// from inside such a "review-shell".
 fn goto_crate_src(selector: &opts::CrateSelector, independent: bool) -> Result<()> {
     if env::var(GOTO_ORIGINAL_DIR_ENV).is_ok() {
         bail!("You're already in a `cargo crev goto` shell");
@@ -247,17 +248,18 @@ fn goto_crate_src(selector: &opts::CrateSelector, independent: bool) -> Result<(
         .name
         .clone()
         .ok_or_else(|| format_err!("Crate name argument required"))?;
-    let (pkg_dir, crate_version) =
-        repo.find_crate(&name, selector.version.as_deref(), independent)?;
+    let crate_ = repo.find_crate(&name, selector.version.as_deref(), independent)?;
+    let crate_dir = crate_.root();
+    let crate_version = crate_.version();
 
     let shell = env::var_os("SHELL").ok_or_else(|| format_err!("$SHELL not set"))?;
     let cwd = env::current_dir()?;
 
-    eprintln!("Opening shell in: {}", pkg_dir.display());
+    eprintln!("Opening shell in: {}", crate_dir.display());
     eprintln!("Use `exit` or Ctrl-D to return to the original project.",);
     eprintln!("Use `review` and `flag` without any arguments to review this crate.");
     let status = process::Command::new(shell)
-        .current_dir(pkg_dir)
+        .current_dir(crate_dir)
         .env(GOTO_ORIGINAL_DIR_ENV, cwd)
         .env(GOTO_CRATE_NAME_ENV, name)
         .env(GOTO_CRATE_VERSION_ENV, &crate_version.to_string())
@@ -270,7 +272,7 @@ fn goto_crate_src(selector: &opts::CrateSelector, independent: bool) -> Result<(
     Ok(())
 }
 
-fn ensure_known_owners_exists(local: &crev_lib::Local) -> Result<()> {
+fn ensure_known_owners_list_exists(local: &crev_lib::Local) -> Result<()> {
     let path = local.get_proofs_dir_path()?.join(KNOWN_CARGO_OWNERS_FILE);
     if !path.exists() {
         crev_common::store_str_to_file(&path, include_str!("known_cargo_owners_defaults.txt"))?;
@@ -280,7 +282,7 @@ fn ensure_known_owners_exists(local: &crev_lib::Local) -> Result<()> {
     Ok(())
 }
 
-fn read_known_owners() -> Result<HashSet<String>> {
+fn read_known_owners_list() -> Result<HashSet<String>> {
     let local = Local::auto_create_or_open()?;
     let content = if let Some(path) = local.get_proofs_dir_path_opt()? {
         let path = path.join(KNOWN_CARGO_OWNERS_FILE);
@@ -296,24 +298,26 @@ fn read_known_owners() -> Result<HashSet<String>> {
         .collect())
 }
 
-fn edit_known_owners() -> Result<()> {
+fn edit_known_owners_list() -> Result<()> {
     let local = Local::auto_create_or_open()?;
     let path = local.get_proofs_dir_path()?.join(KNOWN_CARGO_OWNERS_FILE);
-    ensure_known_owners_exists(&local)?;
+    ensure_known_owners_list_exists(&local)?;
     crev_lib::util::edit_file(&path)?;
     Ok(())
 }
 
+/// Wipe the crate source, then re-download it
 fn clean_crate(name: &str, version: Option<&str>, independent: bool) -> Result<()> {
     let repo = Repo::auto_open_cwd()?;
-    let (pkg_dir, _crate_version) = repo.find_crate(name, version, independent)?;
+    let crate_ = repo.find_crate(name, version, independent)?;
+    let crate_root = crate_.root();
 
-    assert!(!pkg_dir.starts_with(std::env::current_dir()?));
+    assert!(!crate_root.starts_with(std::env::current_dir()?));
 
-    if pkg_dir.is_dir() {
-        std::fs::remove_dir_all(&pkg_dir)?;
+    if crate_root.is_dir() {
+        std::fs::remove_dir_all(&crate_root)?;
     }
-    let (_pkg_dir, _crate_version) = repo.find_crate(name, version, independent)?;
+    let _crate = repo.find_crate(name, version, independent)?;
     Ok(())
 }
 
@@ -322,7 +326,9 @@ fn clean_crate(name: &str, version: Option<&str>, independent: bool) -> Result<(
 /// * `independent` - the crate might not actually be a dependency
 fn crate_open(name: &str, version: Option<&str>, independent: bool) -> Result<()> {
     let repo = Repo::auto_open_cwd()?;
-    let (pkg_dir, _) = repo.find_crate(name, version, independent)?;
+    let crate_ = repo.find_crate(name, version, independent)?;
+
+    let crate_root = crate_.root();
 
     let status = if cfg!(target_os = "windows") {
         process::Command::new("start")
@@ -334,7 +340,7 @@ fn crate_open(name: &str, version: Option<&str>, independent: bool) -> Result<()
         eprintln!("Unsupported platform. Please submit a PR!");
         process::Command::new("xdg-open")
     }
-    .arg(pkg_dir)
+    .arg(crate_root)
     .status()?;
 
     if !status.success() {
@@ -355,9 +361,11 @@ fn create_review_proof(
     proof_create_opt: &opts::CommonProofCreate,
 ) -> Result<()> {
     let repo = Repo::auto_open_cwd()?;
-    let (pkg_dir, crate_version) = repo.find_crate(name, version, independent)?;
+    let crate_ = repo.find_crate(name, version, independent)?;
+    let crate_root = crate_.root();
+    let crate_version = crate_.version();
 
-    assert!(!pkg_dir.starts_with(std::env::current_dir()?));
+    assert!(!crate_root.starts_with(std::env::current_dir()?));
     let local = Local::auto_open()?;
 
     // to protect from creating a digest from a crate in unclean state
@@ -365,7 +373,7 @@ fn create_review_proof(
     // check if the digest was the same
     // BUG: TODO: https://users.rust-lang.org/t/append-an-additional-extension/23586
     let reviewed_pkg_dir: PathBuf =
-        crev_common::fs::append_to_path(pkg_dir.clone(), ".crev.reviewed");
+        crev_common::fs::append_to_path(crate_root.to_owned(), ".crev.reviewed");
     if reviewed_pkg_dir.is_dir() {
         std::fs::remove_dir_all(&reviewed_pkg_dir)?;
     }
@@ -374,12 +382,16 @@ fn create_review_proof(
     // having the cwd pulled from under them and confusing their
     // shells, we move all the entries in a dir, instead of the whole
     // dir. this is not a perfect solution, but better than nothing.
-    crev_common::fs::move_dir_content(&pkg_dir, &reviewed_pkg_dir)?;
-    let (pkg_dir_second, crate_version_second) = repo.find_crate(name, version, independent)?;
-    assert_eq!(pkg_dir, pkg_dir_second);
+    crev_common::fs::move_dir_content(&crate_root, &reviewed_pkg_dir)?;
+    let crate_second = repo.find_crate(name, version, independent)?;
+    let crate_root_second = crate_second.root();
+    let crate_version_second = crate_second.version();
+
+    assert_eq!(crate_root, crate_root_second);
     assert_eq!(crate_version, crate_version_second);
 
-    let digest_clean = crev_lib::get_recursive_digest_for_dir(&pkg_dir, &cargo_min_ignore_list())?;
+    let digest_clean =
+        crev_lib::get_recursive_digest_for_dir(&crate_root, &cargo_min_ignore_list())?;
     let digest_reviewed =
         crev_lib::get_recursive_digest_for_dir(&reviewed_pkg_dir, &cargo_full_ignore_list())?;
 
@@ -388,13 +400,13 @@ fn create_review_proof(
             "The digest of the reviewed and freshly downloaded crate were different; {} != {}; {} != {}",
             digest_clean,
             digest_reviewed,
-            pkg_dir.display(),
+            crate_root.display(),
             reviewed_pkg_dir.display(),
         );
     }
     std::fs::remove_dir_all(&reviewed_pkg_dir)?;
 
-    let vcs = VcsInfoJson::read_from_crate_dir(&pkg_dir)?;
+    let vcs = VcsInfoJson::read_from_crate_dir(&crate_root)?;
     let id = local.read_current_unlocked_id(&crev_common::read_passphrase)?;
 
     let review = proof::review::PackageBuilder::default()
@@ -458,7 +470,6 @@ fn find_reviews(
 }
 
 fn list_reviews(crate_: &opts::CrateSelector) -> Result<()> {
-    // TODO: take trust params?
     for review in find_reviews(crate_)? {
         println!("{}", review);
     }
@@ -466,6 +477,11 @@ fn list_reviews(crate_: &opts::CrateSelector) -> Result<()> {
     Ok(())
 }
 
+/// Handle the `goto mode` commands
+///
+/// After jumping to a crate with `goto`, the crate is selected
+/// already, and commands like `review` must not be given any arguments
+/// like that.
 fn handle_goto_mode_command<F>(args: &opts::ReviewOrGotoCommon, f: F) -> Result<()>
 where
     F: FnOnce(&str, Option<&str>, bool) -> Result<()>,
@@ -554,7 +570,7 @@ fn run_command(command: opts::Command) -> Result<CommandExitStatus> {
                     eprintln!("Visit https://github.com/dpc/crev/wiki/Proof-Repository for help.");
                 }
                 let local = crev_lib::Local::auto_open()?;
-                let _ = ensure_known_owners_exists(&local);
+                let _ = ensure_known_owners_list_exists(&local);
                 res?;
             }
         },
@@ -570,7 +586,7 @@ fn run_command(command: opts::Command) -> Result<CommandExitStatus> {
                 local.edit_readme()?;
             }
             opts::Edit::Known => {
-                edit_known_owners()?;
+                edit_known_owners_list()?;
             }
         },
         opts::Command::Verify(cmd) => match cmd {
@@ -601,9 +617,9 @@ fn run_command(command: opts::Command) -> Result<CommandExitStatus> {
                     );
                     eprintln!(" {:<19} {:<15}", "crate", "version");
                 }
-                let known_owners = read_known_owners().unwrap_or_else(|_| HashSet::new());
+                let known_owners = read_known_owners_list().unwrap_or_else(|_| HashSet::new());
                 let mut total_verification_successful = true;
-                repo.for_every_non_local_dependency_dir(|pkg| {
+                repo.for_every_non_local_dep_crate(|pkg| {
                     let pkg_id = pkg.package_id();
                     let pkg_name = pkg_id.name().as_str();
                     let pkg_version = pkg_id.version().to_string();
