e6426817621b2a06b350707d6eac88af918d7b84
Ossi Herrala <oherrala@iki.fi>
2019-04-23 13:09:56 +0300

    Update tokei

--- cargo-crev/src/tokei.rs
+++ cargo-crev/src/tokei.rs
@@ -1,15 +1,13 @@
 use crate::prelude::*;
 use std::path::Path;
-use tokei::{LanguageType, Languages};
+use tokei::{Config, LanguageType, Languages};
 
 pub fn get_rust_line_count(path: &Path) -> Result<usize> {
-    let path = format!("{}", path.display());
-    let paths = &[path.as_str()];
-    let excluded = vec![];
+    let excluded = &[];
+    let config = Config::default();
     let mut languages = Languages::new();
-    languages.get_statistics(paths, excluded, None);
-    let language_map = languages.remove_empty();
-    let rust = language_map
+    languages.get_statistics(&[path], excluded, &config);
+    let rust = languages
         .get(&LanguageType::Rust)
         .ok_or_else(|| format_err!("Rust should work"))?;
     Ok(rust.code)
