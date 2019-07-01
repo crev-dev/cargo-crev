0202802d03a03a0302ee1d30563ada22998aa5f6
Dawid Ciężarkiewicz <dpc@dpc.pw>
2019-06-30 23:02:52 -0700

    `cargo fmt --all`

--- crev-lib/src/tests/issues.rs
+++ crev-lib/src/tests/issues.rs
@@ -1,10 +1,10 @@
 use super::*;
 
-use crev_data::{proof, OwnId};
-use semver::Version;
-use ifmt::iformat;
 use crev_data::review::{Advisory, AdvisoryRange, Issue};
 use crev_data::TrustLevel;
+use crev_data::{proof, OwnId};
+use ifmt::iformat;
+use semver::Version;
 
 const SOURCE: &str = "SOURCE_ID";
 const NAME: &str = "name";
@@ -12,20 +12,24 @@ const NAME: &str = "name";
 fn build_advisory(id: impl Into<String>, range: AdvisoryRange) -> Advisory {
     let id = id.into();
     Advisory::builder()
-            .range(range)
-            .ids(vec![id.clone()])
-            .comment(iformat!("comment for {id}"))
-            .build()
+        .range(range)
+        .ids(vec![id.clone()])
+        .comment(iformat!("comment for {id}"))
+        .build()
 }
 
 fn build_issue(id: impl Into<String>) -> Issue {
     let id = id.into();
     Issue::builder()
-            .id(id.clone())
-            .comment(iformat!("issue {id}"))
-            .build()
+        .id(id.clone())
+        .comment(iformat!("issue {id}"))
+        .build()
 }
-fn build_proof_with_advisories(id: &OwnId, version: Version, advisories: Vec<Advisory>) -> proof::Proof {
+fn build_proof_with_advisories(
+    id: &OwnId,
+    version: Version,
+    advisories: Vec<Advisory>,
+) -> proof::Proof {
     let package_info = proof::PackageInfo {
         id: None,
         source: "SOURCE_ID".to_owned(),
@@ -76,28 +80,32 @@ fn build_proof_with_issues(id: &OwnId, version: Version, issues: Vec<Issue>) -> 
 fn advisories_sanity() -> Result<()> {
     let id = OwnId::generate_for_git_url("https://a");
 
-/*
-    let package_info = proof::PackageInfo {
-        id: None,
-        source: "SOURCE_ID".to_owned(),
-        name: NAME.into(),
-        version: Version::parse("1.2.3").unwrap(),
-        digest: vec![0, 1, 2, 3],
-        digest_type: proof::default_digest_type(),
-        revision: "".into(),
-        revision_type: proof::default_revision_type(),
-    };
-    let review = proof::review::PackageBuilder::default()
-        .from(id.id.to_owned())
-        .package(package_info.clone())
-        .comment("comment".into())
-        .advisories()
-        .build()
-        .unwrap();
-    let proof = review.sign_by(&id)?;
+    /*
+        let package_info = proof::PackageInfo {
+            id: None,
+            source: "SOURCE_ID".to_owned(),
+            name: NAME.into(),
+            version: Version::parse("1.2.3").unwrap(),
+            digest: vec![0, 1, 2, 3],
+            digest_type: proof::default_digest_type(),
+            revision: "".into(),
+            revision_type: proof::default_revision_type(),
+        };
+        let review = proof::review::PackageBuilder::default()
+            .from(id.id.to_owned())
+            .package(package_info.clone())
+            .comment("comment".into())
+            .advisories()
+            .build()
+            .unwrap();
+        let proof = review.sign_by(&id)?;
 
-*/
-    let proof = build_proof_with_advisories(&id, Version::parse("1.2.3").unwrap(), vec![build_advisory("someid", AdvisoryRange::Major)]);
+    */
+    let proof = build_proof_with_advisories(
+        &id,
+        Version::parse("1.2.3").unwrap(),
+        vec![build_advisory("someid", AdvisoryRange::Major)],
+    );
 
     let mut trustdb = ProofDB::new();
     trustdb.import_from_iter(vec![proof].into_iter());
@@ -126,7 +134,7 @@ fn advisories_sanity() -> Result<()> {
             .len(),
         0
     );
-/*
+    /*
     let review = proof::review::PackageBuilder::default()
         .from(id.id.to_owned())
         .package(package_info)
@@ -137,8 +145,11 @@ fn advisories_sanity() -> Result<()> {
 
     let proof = review.sign_by(&id)?;
     */
-    let proof = build_proof_with_advisories(&id, Version::parse("1.2.3").unwrap(), vec![build_advisory("someid", AdvisoryRange::All)]);
-
+    let proof = build_proof_with_advisories(
+        &id,
+        Version::parse("1.2.3").unwrap(),
+        vec![build_advisory("someid", AdvisoryRange::All)],
+    );
 
     trustdb.import_from_iter(vec![proof].into_iter());
 
@@ -195,95 +206,182 @@ fn issues_sanity() -> Result<()> {
     let proof = review.sign_by(&id)?;
         */
 
-
-    let proof = build_proof_with_advisories(&id, Version::parse("1.2.3").unwrap(), vec![build_advisory("issueX", AdvisoryRange::Major)]);
+    let proof = build_proof_with_advisories(
+        &id,
+        Version::parse("1.2.3").unwrap(),
+        vec![build_advisory("issueX", AdvisoryRange::Major)],
+    );
     trustdb.import_from_iter(vec![proof].into_iter());
 
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("2.0.0").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("2.0.0").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         0
     );
 
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("1.0.1").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("1.0.1").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         1
     );
 
-    let proof = build_proof_with_advisories(&id, Version::parse("2.0.1").unwrap(), vec![build_advisory("issueY", AdvisoryRange::All)]);
+    let proof = build_proof_with_advisories(
+        &id,
+        Version::parse("2.0.1").unwrap(),
+        vec![build_advisory("issueY", AdvisoryRange::All)],
+    );
     trustdb.import_from_iter(vec![proof].into_iter());
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("0.0.1").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("0.0.1").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         1
     );
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("2.0.0").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("2.0.0").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         1
     );
 
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("2.0.1").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("2.0.1").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         0
     );
 
-    let proof = build_proof_with_issues(&id, Version::parse("3.0.5").unwrap(), vec![build_issue("issueX")]);
+    let proof = build_proof_with_issues(
+        &id,
+        Version::parse("3.0.5").unwrap(),
+        vec![build_issue("issueX")],
+    );
     trustdb.import_from_iter(vec![proof].into_iter());
 
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("3.0.4").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("3.0.4").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         0
     );
 
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("3.0.5").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("3.0.5").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         1
     );
 
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("3.1.0").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("3.1.0").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         1
     );
 
-    let proof = build_proof_with_advisories(&id, Version::parse("3.1.0").unwrap(), vec![build_advisory("issueX", AdvisoryRange::Major)]);
+    let proof = build_proof_with_advisories(
+        &id,
+        Version::parse("3.1.0").unwrap(),
+        vec![build_advisory("issueX", AdvisoryRange::Major)],
+    );
     trustdb.import_from_iter(vec![proof].into_iter());
 
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("3.1.0").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("3.1.0").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         0
     );
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("4.0.0").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("4.0.0").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         0
     );
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("3.0.5").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("3.0.5").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         1
     );
 
     assert_eq!(
         trustdb
-            .get_issues_for_version(SOURCE, NAME, &Version::parse("3.0.7").unwrap(), &trust_set, TrustLevel::Medium)
+            .get_issues_for_version(
+                SOURCE,
+                NAME,
+                &Version::parse("3.0.7").unwrap(),
+                &trust_set,
+                TrustLevel::Medium
+            )
             .len(),
         1
     );
