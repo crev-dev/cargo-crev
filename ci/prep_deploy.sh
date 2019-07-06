3ca93d2337cdc6976a0f159d11f149e9d78fa003
Dawid Ciężarkiewicz <dpc@dpc.pw>
2019-07-05 23:13:57 -0700

    Fix `-` handling

--- ci/prep_deploy.sh
+++ ci/prep_deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 name="$CRATE_NAME-$TRAVIS_TAG-$TARGET"
-mkdir $name
-cp target/$TARGET/release/$CRATE_NAME $name/
-cp README.md LICENSE* $name/
-tar czvf $name.tar.gz $name
+mkdir "$name"
+cp "target/$TARGET/release/$CRATE_NAME" "$name/"
+cp README.md LICENSE* "$name/"
+tar czvf "$name.tar.gz" "$name"
 
 # Get the sha-256 checksum w/o filename and newline
-echo -n $(shasum -ba 256 "$name.tar.gz" | cut -d " " -f 1) > $name.tar.gz.sha256
+echo -n $(shasum -ba 256 "$name.tar.gz" | cut -d " " -f 1) > "$name.tar.gz.sha256"
 
