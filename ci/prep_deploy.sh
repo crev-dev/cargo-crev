240814ef6f1578ccf64bb4d1872b7fee24608d5d
Dawid Ciężarkiewicz <dpc@dpc.pw>
2019-07-06 13:26:53 -0700

    Release `cargo-crev`'s README, not the main one

--- ci/prep_deploy.sh
+++ ci/prep_deploy.sh
@@ -6,7 +6,7 @@ export CRATE_NAME=cargo-crev
 name="$TRAVIS_TAG-$TARGET"
 mkdir -p "$name"
 cp "target/$TARGET/release/$CRATE_NAME" "$name/"
-cp README.md LICENSE* "$name/"
+cp cargo-crev/README.md LICENSE* "$name/"
 tar czvf "$name.tar.gz" "$name"
 
 # Get the sha-256 checksum w/o filename and newline
