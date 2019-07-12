3762028aa564e03c419c874b22268dfb76388aab
Dawid Ciężarkiewicz <dpc@dpc.pw>
2019-07-11 23:36:31 -0700

    Make a fake library so docs.rs renders the documentation

---
+++ cargo-crev/src/lib.rs
@@ -0,0 +1,6 @@
+// this is a dummy library just to make docs.rs render what I need
+#![cfg_attr(
+    feature = "documentation",
+    doc = "See the [user documentation module](./doc/user/index.html) and in particular the [Getting Started Guide](./doc/user/getting_started/index.html)."
+)]
+#![cfg_attr(feature = "documentation", feature(external_doc))]
