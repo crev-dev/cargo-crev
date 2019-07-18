4ac135125ddbfa48f153cac4257a95af5bd01e41
Canop <cano.petrole@gmail.com>
2019-07-18 15:09:37 +0200

    verify dep computing done in another thread
    and results sent using a channel

--- cargo-crev/src/dep/computer.rs
+++ cargo-crev/src/dep/computer.rs
@@ -1,14 +1,26 @@
+use rayon::prelude::*;
+use cargo::core::{
+    package::{Package},
+    package_id::PackageId,
+};
 use crev_common::convert::OptionDeref;
 use crev_lib;
 use std::{
     collections::HashSet,
     default::Default,
     path::PathBuf,
+    thread,
 };
+use crossbeam:: {
+    self,
+    channel::{Sender, Receiver, unbounded},
+};
+
 
 use crate::prelude::*;
 use crate::crates_io;
 use crate::opts::*;
+use crate::repo::*;
 use crate::shared::*;
 use crate::tokei;
 
@@ -16,11 +28,49 @@ use crate::dep::dep::*;
 
 use crev_lib::{*, proofdb::*};
 
-/// manages most analysis of a crate dependency.
-///
-/// This excludes:
-/// - downloading it
-/// - computing the geiger count
+
+/// a structure internally used by the computer
+struct DepRow {
+    id: PackageId, // contains the name, version
+    pub root: PathBuf,
+    pub geiger_count: Option<u64>,
+    pub has_custom_build: bool,
+}
+impl DepRow {
+    pub fn from(pkg: & Package) -> Self {
+        let id = pkg.package_id();
+        let root = pkg.root().to_path_buf();
+        let has_custom_build = pkg.has_custom_build();
+        DepRow {
+            id,
+            root,
+            has_custom_build,
+            geiger_count: None,
+        }
+    }
+
+    pub fn download_if_needed(
+        &mut self,
+    ) -> Result<()> {
+        if !self.root.exists() {
+            let repo = Repo::auto_open_cwd()?;
+            let mut source = repo.load_source()?;
+            source.download(self.id)?;
+        }
+        Ok(())
+    }
+
+    pub fn count_geiger(
+        &mut self,
+    ) {
+        debug_assert!(self.root.exists());
+        self.geiger_count = get_geiger_count(&self.root).ok();
+    }
+
+
+}
+
+/// manages analysis of a crate dependency.
 pub struct DepComputer {
     db: ProofDB,
     trust_set: TrustSet,
@@ -31,6 +81,7 @@ pub struct DepComputer {
     skip_verified: bool,
     skip_known_owners: bool,
 }
+
 
 impl DepComputer {
 
@@ -62,23 +113,132 @@ impl DepComputer {
         })
     }
 
-    fn try_compute(
-        &mut self,
-        row: &mut DepRow,
-    ) -> Result<Option<Dep>> {
+    /// start computations on a new thread, and return
+    /// - a channel receiver, to get new events
+    /// - a channel sender, to ask for computation stop
+    pub fn run_computation(self) -> Receiver<ComputationEvent> {
+        let (tx_events, rx_events) = unbounded();
+        thread::spawn(move || {
+            match self.compute_all(tx_events) {
+                Ok(()) => {
+                    println!("OK - computation done"); // we need a better logging
+                }
+                Err(e) => {
+                    println!("NOT OK: {:?}", e);
+                }
+            }
+        });
+        rx_events
+    }
 
+    /// do the computation
+    fn compute_all(mut self, tx_events: Sender<ComputationEvent>) -> Result<()> {
+        let status = TableComputationStatus::New;
+        tx_events.send(ComputationEvent::from_status(status)).unwrap();
+
+        let repo = Repo::auto_open_cwd()?;
+        let package_set = repo.non_local_dep_crates()?;
+        let pkgs = package_set.get_many(package_set.package_ids())?;
+        let mut rows: Vec<DepRow> = pkgs.iter()
+            .filter(|pkg| pkg.summary().source_id().is_registry())
+            .map(|pkg| DepRow::from(pkg))
+            .collect();
+
+        // computing in parallel (using rayon) all the geiger things
+        // (which are costly)
+        let (tx_geiger, rx_geiger) = unbounded();
+        let mut progress = Progress {
+            done: 0,
+            total: rows.len(),
+        };
+
+        // we could probably avoid this thread and send the computation event from the closure
+        // given to rayon but sending the correct progress value would involve a mutex, I think
+        let tx_geiger_events = tx_events.clone();
+        thread::spawn(move|| {
+            loop {
+                let b = rx_geiger.recv().unwrap();
+                progress.done += 1;
+                let status = TableComputationStatus::ComputingGeiger { progress };
+                tx_geiger_events.send(ComputationEvent::from_status(status)).unwrap();
+                if progress.is_complete() {
+                    break;
+                }
+            }
+        });
+        rows
+            .par_iter_mut()
+            .for_each(|row| {
+                row.download_if_needed().unwrap();
+                row.count_geiger();
+                tx_geiger.send(true).unwrap();
+            });
+
+        // doing the rest of the computation
+        progress.done = 0;
+        let status = TableComputationStatus::ComputingTrust { progress };
+        tx_events.send(ComputationEvent::from_status(status)).unwrap();
+        for row in rows.drain(..) {
+            let dep = self.compute_dep(row);
+            progress.done += 1;
+            let status = TableComputationStatus::ComputingTrust{ progress };
+            let event = ComputationEvent {
+                computation_status: status,
+                finished_dep: Some(dep),
+            };
+            tx_events.send(event).unwrap();
+        }
+
+        // all done
+        let status = TableComputationStatus::Done;
+        tx_events.send(ComputationEvent::from_status(status)).unwrap();
+        Ok(())
+    }
+
+    fn compute_dep(
+        &mut self,
+        row: DepRow,
+    ) -> Dep {
+        let name = row.id.name().as_str().to_string();
+        let version = row.id.version().clone();
+        let computation_status = match self.try_compute_dep(&row) {
+            Ok(Some(computed_dep)) => {
+                DepComputationStatus::Ok{
+                    computed_dep,
+                }
+            }
+            Ok(None) => {
+                DepComputationStatus::Skipped
+            }
+            Err(e) => {
+                println!("Computation Failed: {:?}", e);
+                DepComputationStatus::Failed
+            }
+        };
+        Dep {
+            name,
+            version,
+            computation_status,
+            root: row.root,
+            geiger_count: row.geiger_count,
+            has_custom_build: row.has_custom_build,
+        }
+    }
+
+    fn try_compute_dep(
+        &mut self,
+        row: &DepRow,
+    ) -> Result<Option<ComputedDep>> {
         let crate_id = row.id;
         let name = crate_id.name().as_str().to_string();
         let version = crate_id.version();
         let crate_root = &row.root;
         let digest = crev_lib::get_dir_digest(&crate_root, &self.ignore_list)?;
-
         let unclean_digest = !is_digest_clean(
             &self.db, &name, &version, &digest
         );
         let result = self.db.verify_package_digest(&digest, &self.trust_set, &self.requirements);
         let verified = result.is_verified();
-
         if verified && self.skip_verified {
             return Ok(None);
         }
@@ -148,10 +308,8 @@ impl DepComputer {
             &name,
             &self.requirements,
         );
-        Ok(Some(Dep {
+        Ok(Some(ComputedDep {
             digest,
-            name,
-            version: version.clone(),
             latest_trusted_version,
             trust: result,
             reviews,
@@ -159,28 +317,11 @@ impl DepComputer {
             owners,
             issues,
             loc,
-            has_custom_build: row.has_custom_build,
             unclean_digest,
             verified,
         }))
     }
 
-    pub fn compute(
-        &mut self,
-        row: &mut DepRow,
-    ) {
-        row.computation_status = RowComputationStatus::InProgress;
-        match self.try_compute(row) {
-            Ok(Some(dep)) => {
-                row.computation_status = RowComputationStatus::Ok{dep};
-            }
-            Ok(None) => {
-                row.computation_status = RowComputationStatus::Skipped;
-            }
-            Err(e) => {
-                row.computation_status = RowComputationStatus::Failed;
-                println!("Computation Failed: {:?}", e);
-            }
-        }
-    }
 }
+
+
--- cargo-crev/src/dep/dep.rs
+++ cargo-crev/src/dep/dep.rs
@@ -1,14 +1,5 @@
-use cargo::core::{
-    package::{Package, PackageSet},
-    package_id::PackageId,
-};
 use semver::Version;
 use std::path::PathBuf;
-
-use crate::prelude::*;
-use crate::term::{self, *};
-use crate::repo::*;
-use crate::shared::*;
 
 use crev_data::*;
 use crev_lib::*;
@@ -24,23 +15,40 @@ impl Progress {
     }
 }
 
+/// Current state/progress of the computation
+///  of the repo's dependencies
 #[derive(Clone, Copy, Debug)]
 pub enum TableComputationStatus {
     New,
     ComputingGeiger {
         progress: Progress,
     },
-    Done,
+    ComputingTrust {
+        progress: Progress,
+    },
+    Done, // might be a crash, too
 }
-impl TableComputationStatus {
 
+/// Events are the output of the computer, this is
+///  where the computed data are to be read.
+pub struct ComputationEvent {
+    pub computation_status: TableComputationStatus,
+    pub finished_dep: Option<Dep>,
+}
+impl ComputationEvent {
+    pub fn from_status(computation_status: TableComputationStatus) -> Self {
+        Self {
+            computation_status,
+            finished_dep: None,
+        }
+    }
 }
 
-pub enum RowComputationStatus {
-    New,
-    InProgress,
+/// Status of the computation of a dependency. If it's OK contains
+///  the computed dependency.
+pub enum DepComputationStatus {
     Ok {
-        dep: Dep,
+        computed_dep: ComputedDep,
     },
     Skipped, // imply it's verified and args ask for skipping of verified
     Failed,
@@ -57,10 +65,8 @@ pub struct TrustCount {
 
 /// The computed content for a dep. One field should be one
 ///  cell in the displayed dep table
-pub struct Dep {
+pub struct ComputedDep {
     pub digest: Digest,
-    pub name: String,
-    pub version: Version,
     pub latest_trusted_version: Option<Version>,
     pub trust: VerificationStatus,
     pub reviews: CrateCounts,
@@ -68,226 +74,58 @@ pub struct Dep {
     pub owners: Option<TrustCount>,
     pub issues: TrustCount,
     pub loc: Option<usize>,
-    pub has_custom_build: bool, // duplicate data, improve that
     pub unclean_digest: bool,
     pub verified: bool,
 }
 
-pub struct DepRow {
-    pub id: PackageId, // contains the name, version
+/// A dependency, as returned by the computer. It may
+///  contain (depending on success/slipping) the computed
+///  dep.
+pub struct Dep {
+    pub name: String,
+    pub version: Version,
+    pub computation_status: DepComputationStatus,
     pub root: PathBuf,
-    pub has_custom_build: bool,
     pub geiger_count: Option<u64>,
-    pub computation_status: RowComputationStatus,
+    pub has_custom_build: bool,
 }
 
 impl Dep {
-    pub fn term_print(
-        &self,
-        term: &mut Term,
-        geiger_count: Option<u64>,
-        verbose: bool,
-    ) -> Result<()> {
-        if verbose {
-            print!("{:43} ", self.digest);
-        }
-        term.print(
-            format_args!("{:6}", self.trust),
-            term::verification_status_color(&self.trust),
-        )?;
-        print!(" {:2} {:2}", self.reviews.version, self.reviews.total);
-        if let Some(downloads) = &self.downloads {
-            term.print(
-                format_args!(" {:>8}", downloads.version),
-                if downloads.version < 1000 {
-                    Some(::term::color::YELLOW)
-                } else {
-                    None
-                },
-            )?;
-            term.print(
-                format_args!(" {:>9}", downloads.total),
-                if downloads.total < 10000 {
-                    Some(::term::color::YELLOW)
-                } else {
-                    None
-                },
-            )?;
-        } else {
-            println!(" {:>8} {:>9}", "?", "?");
-        }
-        if let Some(owners) = &self.owners {
-            term.print(
-                format_args!(" {}", owners.trusted),
-                term::known_owners_count_color(owners.trusted)
-            )?;
-            term.print(
-                format_args!(" {}", owners.total),
-                term::known_owners_count_color(owners.total)
-            )?;
-        } else {
-            println!(" ???");
-        }
-
-        term.print(
-            format_args!("{:4}", self.issues.trusted),
-            if self.issues.trusted > 0 {
-                Some(::term::color::RED)
-            } else {
-                None
-            },
-        )?;
-        print!("/");
-        term.print(
-            format_args!("{:<2}", self.issues.total),
-            if self.issues.total > 0 {
-                Some(::term::color::YELLOW)
-            } else {
-                None
-            },
-        )?;
-        match self.loc {
-            Some(loc) => print!(" {:>6}", loc),
-            None => print!(" {:>6}", "err"),
-        }
-        match geiger_count {
-            Some(geiger_count) => print!(" {:>7}", geiger_count),
-            None => print!(" {:>7}", "err"),
-        }
-        term.print(
-            format_args!(" {:4}", if self.has_custom_build { "CB" } else { "" }),
-            ::term::color::YELLOW,
-        )?;
-        print!(
-            " {:<20} {:<15}",
-            self.name,
-            pad_left_manually(self.version.to_string(), 15)
-        );
-
-        print!(
-            " {}",
-            latest_trusted_version_string(
-                &self.version,
-                &self.latest_trusted_version
-            )
-        );
-        Ok(())
-    }
-
-}
-
-
-impl DepRow {
-    pub fn from(pkg: & Package) -> Self {
-        let id = pkg.package_id();
-        let root = pkg.root().to_path_buf();
-        let has_custom_build = pkg.has_custom_build();
-        DepRow {
-            id,
-            root,
-            has_custom_build,
-            geiger_count: None,
-            computation_status: RowComputationStatus::New,
-        }
-    }
-
-    pub fn download_if_needed(
-        &mut self,
-    ) -> Result<()> {
-        if !self.root.exists() {
-            let repo = Repo::auto_open_cwd()?;
-            let mut source = repo.load_source()?;
-            source.download(self.id)?;
-        }
-        Ok(())
-    }
-
-    pub fn count_geiger(
-        &mut self,
-    ) {
-        debug_assert!(self.root.exists());
-        self.geiger_count = get_geiger_count(&self.root).ok();
-    }
 
     pub fn is_digest_unclean(&self) -> bool {
         match &self.computation_status {
-            RowComputationStatus::Ok{dep} => dep.unclean_digest,
+            DepComputationStatus::Ok{computed_dep} => computed_dep.unclean_digest,
             _ => false,
         }
     }
-
-    pub fn term_print_header(_term: &mut Term, verbose: bool) {
-        if verbose {
-            eprint!("{:43} ", "digest");
-        }
-        eprint!(
-            "{:6} {:8} {:^15} {:4} {:6} {:6} {:6} {:4}",
-            "status",
-            "reviews",
-            "downloads",
-            "own.",
-            "issues",
-            "lines",
-            "geiger",
-            "flgs"
-        );
-        eprintln!(" {:<20} {:<15} {:<15}", "crate", "version", "latest_t");
-    }
-
-    pub fn term_print(&self, term: &mut Term, verbose: bool) -> Result<()> {
-        match &self.computation_status {
-            RowComputationStatus::New => {
-                println!("not yet computed");
-            }
-            RowComputationStatus::InProgress => {
-                println!("in progress...");
-            }
-            RowComputationStatus::Failed => {
-                println!("computation failed"); // TODO write the name
-            }
-            RowComputationStatus::Skipped => {
-                println!("skipped"); // TODO write the name
-            }
-            RowComputationStatus::Ok{dep} => {
-                dep.term_print(term, self.geiger_count, verbose)?;
-                println!();
-            }
-        }
-        Ok(())
-    }
 }
 
-
 pub struct DepTable {
-    pub rows: Vec<DepRow>,
+    pub deps: Vec<Dep>, // accumulates the deps sent by the computer
     pub computation_status: TableComputationStatus,
 }
 impl DepTable {
-    pub fn new(package_set: &PackageSet<'_>) -> Result<DepTable> {
-        let pkgs = package_set.get_many(package_set.package_ids())?;
-        let rows = pkgs.iter()
-            .filter(|pkg| pkg.summary().source_id().is_registry())
-            .map(|pkg| DepRow::from(pkg))
-            .collect();
-        Ok(DepTable {
-            rows,
+    pub fn new() -> DepTable {
+        DepTable {
+            deps: Vec::new(),
             computation_status: TableComputationStatus::New,
-        })
+        }
+    }
+    pub fn update(&mut self, event: ComputationEvent) {
+        self.computation_status = event.computation_status;
+        if let Some(dep) = event.finished_dep {
+            self.deps.push(dep);
+        }
+    }
+    pub fn is_computation_finished(&self) -> bool {
+        match &self.computation_status {
+            TableComputationStatus::Done => true,
+            _ => false,
+        }
     }
 }
 
-fn pad_left_manually(s: String, width: usize) -> String {
-    if s.len() <= width {
-        let padding = std::iter::repeat(" ")
-            .take(width - s.len())
-            .collect::<String>();
-        format!("{}{}", s, padding)
-    } else {
-        s
-    }
-}
-
-fn latest_trusted_version_string(
+pub fn latest_trusted_version_string(
     base_version: &Version,
     latest_trusted_version: &Option<Version>,
 ) -> String {
