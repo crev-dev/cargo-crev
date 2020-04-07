21b0dafdbbef525828dd6711f5cadbc3758b812f
Kornel <kornel@geekhood.net>
2020-04-07 10:55:26 +0100

    OpenSSL 1.0.x is dead

--- ci/travis-musl-openssl.sh
+++ ci/travis-musl-openssl.sh
@@ -11,8 +11,8 @@ case "$TARGET" in
 esac
 
 rustup target add "$TARGET"
-curl https://www.openssl.org/source/openssl-1.0.2l.tar.gz | tar xzf -
-cd openssl-1.0.2l
+curl https://www.openssl.org/source/openssl-1.1.1f.tar.gz | tar xzf -
+cd openssl-1.1.1f
 CC=musl-gcc ./Configure --prefix="$OPENSSL_DIR" no-dso no-ssl2 no-ssl3 "${OPTIONS[@]}" -fPIC
 make -j"$(nproc)"
 make install
