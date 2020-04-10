8619dabd9c8c6c1bc1432783368c6d7f73a3ac98
Kornel <kornel@geekhood.net>
2020-04-10 01:10:23 +0100

    Fix MUSL

--- ci/travis-musl-openssl.sh
+++ ci/travis-musl-openssl.sh
@@ -13,6 +13,6 @@ esac
 rustup target add "$TARGET"
 curl https://www.openssl.org/source/openssl-1.1.1f.tar.gz | tar xzf -
 cd openssl-1.1.1f
-CC=musl-gcc ./Configure --prefix="$OPENSSL_DIR" no-dso no-ssl2 no-ssl3 "${OPTIONS[@]}" -fPIC
+CC=musl-gcc ./Configure "-idirafter /usr/include/" "-idirafter /usr/include/x86_64-linux-gnu/" --prefix="$OPENSSL_DIR" no-dso no-ssl2 no-ssl3 "${OPTIONS[@]}" -fPIC
 make -j"$(nproc)"
 make install
