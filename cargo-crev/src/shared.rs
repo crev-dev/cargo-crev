crev-dev/cargo-crev

commit 68656a39938cc280e99580ff063b7af93d08c7cd

Add `-u`/`--unrelated` to `crate diff`

cargo-crev/src/shared.rs
@@ -473,7 +473,7 @@ pub fn run_diff(args: &opts::Diff) -> Result<std::process::ExitStatus> {
     let name = &args.name;
 
     let dst_version = &args.dst;
-    let dst_crate_id = repo.find_pkgid(name, dst_version.to_owned().as_ref(), false)?;
+    let dst_crate_id = repo.find_pkgid(name, dst_version.to_owned().as_ref(), args.unrelated)?;
     let dst_crate = repo.get_crate(&dst_crate_id)?;
 
     let requirements = crev_lib::VerificationRequirements::from(args.requirements.clone());
@@ -496,7 +496,7 @@ pub fn run_diff(args: &opts::Diff) -> Result<std::process::ExitStatus> {
             )
         })
         .ok_or_else(|| format_err!("No previously reviewed version found"))?;
-    let src_crate_id = repo.find_pkgid(name, Some(&src_version), true)?;
+    let src_crate_id = repo.find_pkgid(name, Some(&src_version), args.unrelated)?;
     let src_crate = repo.get_crate(&src_crate_id)?;
 
     local.record_review_activity(

cargo-crev/src/opts.rs
@@ -184,6 +184,10 @@ pub struct Diff {
     #[structopt(long = "dst")]
     pub dst: Option<Version>,
 
+    /// This crate is not neccesarily a dependency of the current cargo project
+    #[structopt(long = "unrelated", short = "u")]
+    pub unrelated: bool,
+
     #[structopt(flatten)]
     pub requirements: VerificationRequirements,
 

commit a6f1f121adffe1aac741d74689858bddfb3714b8

Fix `cargo crev crate diff` without `-u`
Broken in 68656a3

Fix #485

cargo-crev/src/shared.rs
@@ -496,7 +496,7 @@ pub fn run_diff(args: &opts::Diff) -> Result<std::process::ExitStatus> {
             )
         })
         .ok_or_else(|| format_err!("No previously reviewed version found"))?;
-    let src_crate_id = repo.find_pkgid(name, Some(&src_version), args.unrelated)?;
+    let src_crate_id = repo.find_pkgid(name, Some(&src_version), true)?;
     let src_crate = repo.get_crate(&src_crate_id)?;
 
     local.record_review_activity(