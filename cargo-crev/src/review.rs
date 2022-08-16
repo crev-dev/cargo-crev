a5484e1288df40c4f637f06523b17f29d8319d29
Chris Morgan <me@chrismorgan.info>
2022-08-16 15:11:07 +1000

    Relax the crate review working directory check
    Allow current_dir to match crate_dir, since check_package_clean_state
    basically goes out of its way to make sure this works.
    
    Also document in detail why and how this check is done, since it was not
    at all clear to me initially, and indeed I realised the check was
    partially broken when I tried to understand how the working directory
    had been changed already in the process (the GOTO_ORIGINAL_DIR thing).
    
    I’d like to fix the FIXME I’ve left in there, but it requires a more
    complicated refactoring that I’m not inclined to do personally right
    now. I never did like working directory shenanigans outside of shell
    scripts. Problematic global state and all that.
    
    The *code* in this change should make a fairly niche case work,
    but really its purpose is as much about documenting the check as
    anything else, and preparing for fixing the broken goto shell
    subdirectory thing.

--- cargo-crev/src/review.rs
+++ cargo-crev/src/review.rs
@@ -36,9 +36,26 @@ pub fn create_review_proof(
     let crate_root = crate_.root();
     let effective_crate_version = crate_.version();
 
+    // We check the working directory because of how check_package_clean_state modifies the
+    // contents of the crate root, moving everything out of the directory.
+    // Therefore, it’s acceptable to run from the crate root directory or from outside the crate
+    // root directory, but not from a subdirectory of the crate root, because it may fail or
+    // misbehave on some platforms and is likely to exhibit confusing behaviour elsewhere.
+    //
+    // (While talking of problematic working directories: the "{crate_root}.crev.reviewed"
+    // directory would be bad, but is not trivially (in lines of code) to detect since
+    // OsStr::starts_with doesn’t exist; and symlinks can probably wreak havoc. But neither of
+    // these are likely to be encountered accidentally, so they’re not worth worrying about.)
+    //
+    // FIXME: in goto shells, cwd has already been changed to the GOTO_ORIGINAL_DIR,
+    // but we should actually be checking the process’s original cwd, either instead of or as well
+    // as, as that’s the cwd from the *user’s* perspective. As it stands, you could easily goto a
+    // crate, switch to a subdirectory, review, and either have fs errors (Windows, probably) or be
+    // returned to a deleted directory (most other OSes).
+    let cwd = std::env::current_dir()?;
     assert!(
-        !std::env::current_dir()?.starts_with(crate_root),
-        "Cannot create review proof while inside {}.",
+        !cwd.starts_with(crate_root) || cwd == crate_root,
+        "Cannot create review proof while in a subdirectory of {}.",
         crate_root.display(),
     );
     let local = Local::auto_open()?;
