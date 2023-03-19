4b64a449ca68f02ebc0e06dcbd17f6da0732b7ad
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-03-18 19:30:53 -0700

    chore: update flake dependencies to get a newer rustc version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Cryptographically verifiable Code REviews";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
