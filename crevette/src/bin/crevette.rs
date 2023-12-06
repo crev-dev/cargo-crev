b9bf0c9ee0cc4524ca92c6712e874905144a7a32
Kornel <kornel@geekhood.net>
2023-12-06 00:51:02 +0000

    Write debcargo/guix to file

--- crevette/src/bin/crevette.rs
+++ crevette/src/bin/crevette.rs
@@ -18,11 +18,14 @@ fn main() -> ExitCode {
 }
 
 fn run() -> Result<(), Error> {
+    let mut action: fn(Crevette) -> _ = |c| c.convert_into_repo();
     match std::env::args().nth(1).as_deref() {
         Some("--help") => {
             eprintln!("https://lib.rs/crevette {}
 Run without args to update your crev repo.
-Run with --debcargo to make a vet file from Debian package list.", env!("CARGO_PKG_VERSION"));
+Run with --debcargo to make a vet file from Debian package list.
+Run with --guix to make a vet file from Debian package list.
+", env!("CARGO_PKG_VERSION"));
             return Ok(())
         },
         Some("--debcargo") => {
@@ -32,10 +35,11 @@ Run with --debcargo to make a vet file from Debian package list.", env!("CARGO_P
             }
             #[cfg(feature = "debcargo")]
             {
-                let dirs = directories_next::BaseDirs::new().unwrap();
-                let cache_dir = dirs.cache_dir().join("crevette");
-                println!("{}", Crevette::from_debcargo_repo(&cache_dir)?);
-                return Ok(())
+                action = |c| {
+                    let dirs = directories_next::BaseDirs::new().unwrap();
+                    let cache_dir = dirs.cache_dir().join("crevette");
+                    c.convert_debcargo_repo(&cache_dir)
+                };
             }
         },
         Some("--guix") => {
@@ -45,10 +49,11 @@ Run with --debcargo to make a vet file from Debian package list.", env!("CARGO_P
             }
             #[cfg(feature = "guix")]
             {
-                let dirs = directories_next::BaseDirs::new().unwrap();
-                let cache_dir = dirs.cache_dir().join("crevette");
-                println!("{}", Crevette::from_guix_repo(&cache_dir)?);
-                return Ok(())
+                action = |c| {
+                    let dirs = directories_next::BaseDirs::new().unwrap();
+                    let cache_dir = dirs.cache_dir().join("crevette");
+                    c.convert_guix_repo(&cache_dir)
+                };
             }
         },
         Some(other) => {
@@ -56,7 +61,7 @@ Run with --debcargo to make a vet file from Debian package list.", env!("CARGO_P
         },
         None => {},
     }
-    let res = Crevette::new().and_then(|c| c.convert_into_repo())?;
+    let res = Crevette::new().and_then(action)?;
         println!(
             "Wrote '{}'\nRun `cargo crev publish` to upload the file to {}\nThen run `cargo vet import yourname {}`\n",
             res.local_path.display(),
